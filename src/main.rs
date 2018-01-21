45aca39e774f628ba225f74473b3e0ff71b35e8a
Nicholas Dujay <nickdujay@gmail.com>
2018-01-21 12:42:43 -0500

    add comment explaining what to do

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,20 @@ extern crate ws;
 use std::env;
 use ws::listen;
 
+/*
+function createGame(gameId) {
+  const nsp = io.of(`/games/${gameId}`);
+  nsp.on('connection', socket => {
+    socket.on('chat', payload => {
+      socket.broadcast.emit('chat', payload);
+    });
+    socket.on('game', payload => {
+      socket.broadcast.emit('game', payload);
+    });
+  });
+}
+*/
+
 fn main() {
   let port = env::var("PORT").unwrap_or("5000".to_string());
   let bind_url = format!("0.0.0.0:{}", port);
