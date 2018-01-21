e319ce8936424182d0fe739aa75a1bd87ea8cdcf
Nicholas Dujay <nickdujay@gmail.com>
2018-01-21 11:21:32 -0500

    setup for heroku

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate ws;
+
+use std::env;
+use ws::listen;
+
 fn main() {
-    println!("Hello, world!");
+  let port = env::var("PORT").unwrap_or("5000".to_string());
+  let bind_url = format!("0.0.0.0:{}", port);
+
+  listen(bind_url, |out| {
+    move |msg| {
+      println!("{:?}", msg);
+      out.send(msg)
+    }
+  }).unwrap();
 }
